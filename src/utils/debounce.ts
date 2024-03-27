cbe596b690e279bf88f5828ca51c0210906ddafa
linikers <linikers@hotmail.com>
2024-03-27 15:16:07 -0300

    debounced

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,9 +1,16 @@
 export const debounce = <F extends(...args: any[]) => any> (
     func: F,
+    waitFor: number,
 ) => {
 
-    const debounced = () => {
+    let timeout: ReturnType<typeof setTimeout> | null = null
 
+    const debounced = (...args: Parameters<F>) => {
+        if(timeout !== null) {
+            clearTimeout(timeout)
+            timeout = null
+        }
+        timeout = setTimeout(() => func(...args), waitFor)
     }
-    return debounced
+    return debounced as (...args:Parameters<F>) => ReturnType<F>
 }
