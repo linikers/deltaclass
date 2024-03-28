d061e61df66eec25be3515aeda96e775e4c97fcb
linikers <linikers@hotmail.com>
2024-03-28 14:32:14 -0300

    lscache bucket

--- src/configs/LsCache.ts
+++ src/configs/LsCache.ts
@@ -2,13 +2,26 @@ export class LsCache {
     hasSupport: boolean | undefined;
     hasSupportJson: boolean | undefined;
     type: "local" | "session";
+    private storage: Storage;
+
+    cacheBucket = "";
+    expiryMilliseconds = 60 * 1000;
+    warnings = false;
 
     constructor({
-        type
+        type,
+        cacheBucket,
     } : {
-        type: "local" | "session"
+        type: "local" | "session";
+        cacheBucket?: string;
     }){
         this.type = type
+        if(type == "local") {
+            this.storage = localStorage;
+        } else {
+            this.storage = sessionStorage
+        }
+        if (cacheBucket) this.cacheBucket = this.cacheBucket;
     }
 }
 
