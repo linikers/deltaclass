3d18fbeeaa7c84f681051dcde97cf074e8ed8635
linikers <linikers@hotmail.com>
2024-03-28 11:44:37 -0300

    axios - sem refresh user

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,10 +1,10 @@
 import { snack } from "@/components/GlobalSnackbar";
 import { store } from "@/store";
 import { getError } from "@/utils";
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
+import Axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
 import { IAxiosCacheAdapterOptions, setup } from "axios-cache-adapter";
 import { Environment } from "./Enviroment";
-import { config } from "process";
+
 
 const limit = 5;
 let count = 0;
@@ -55,6 +55,12 @@ export const interceptorError = async (
     }
 }
 
+export const interceptorGenericError = (error: AxiosError) => {
+    //@ts-ignore
+    error.message = error?.response?.data?.error ?? error.message ?? error.toString()
+    return Promise.reject(error)
+}
+
 const invalidate = async (
     config: IAxiosCacheAdapterOptions,
     request: AxiosRequestConfig<any>,
@@ -74,26 +80,62 @@ export const api = setup({
     }
 })
 
-export const apiRelatorio = setup({})
+export const apiRelatorio = setup({
+    baseURL: Environment.RELATORIO_API_URL,
+    cache: {
+        maxAge:Environment.MILLISECONDS_IN_A_SECONDS,
+        exclude: {query: false},
+        invalidate,
+    }
+})
 
 export const apiConfig = setup({
-    baseURL: Environment.CONFIG_DELTA_API_URL
+    baseURL: Environment.CONFIG_DELTA_API_URL,
 })
 
 export const apiPush = setup({
     baseURL: Environment.PUSH_DELTA_API_URL,
 })
 
-export const setToken = () => {
-
+export const setToken = (token: string) => {
+    api.defaults.headers.common.Authorization = `Bearer ${token}`;
+    apiRelatorio.defaults.headers.common.Authorization = `Bearer ${token}`;
+    apiConfig.defaults.headers.common.Authorization = `Bearer ${token}`;
+    apiPush.defaults.headers.common.Accept = `Bearer ${token}`;
 }
 
 export const setBaseUrl = (escolaId: string) => {
     api.defaults.baseURL =  `${Environment.API_URL}/api/${escolaId}`;
-    
-}
+    apiRelatorio.defaults.baseURL = `${Environment.RELATORIO_API_URL}/api/${escolaId}`;
+};
 
 api.interceptors.response.use(
     (success) => success,
     (error) => interceptorError(error, api)
+)
+apiRelatorio.interceptors.response.use(
+    (success) => success,
+    (error) => interceptorError(error, api)
+)
+apiConfig.interceptors.response.use(
+    (success) =>success,
+    (error) => interceptorError(error, api)
+)
+apiPush.interceptors.response.use(
+    (success) => success,
+    (error) => interceptorError(error, api)
+)
+
+api.interceptors.response.use((success) => success, interceptorGenericError);
+apiRelatorio.interceptors.response.use(
+    (success) => success,
+    interceptorGenericError
+)
+apiConfig.interceptors.response.use(
+    (success) => success,
+    interceptorGenericError
+)
+apiPush.interceptors.response.use(
+    (success) => success,
+    interceptorGenericError
 )
